06c33d60908b445c60d7092c219f0d8eac8e59b6
Vadim Mishkhozhev <vadim.stug@gmail.com>
2019-06-24 11:20:57 +0700

    fix overlay modal

--- inc/hooks.php
+++ inc/hooks.php
@@ -8,17 +8,19 @@ function catchpixel_right_fixed_buttons() {
     <a class="order-call" href="#" data-toggle="modal" data-target="#order_call"><span class="icon-tel"></span>Заказать звонок</a>
   </div>
 
-  <div class="modal custom fade" id="signup" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <?php echo do_shortcode( '[contact-form-7 id="' . get_theme_mod ( 'signup' ) . '" title="Записаться"]' ); ?>
+  <div class="modal-popup-wrapper">
+    <div class="modal custom fade" id="signup" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <?php echo do_shortcode( '[contact-form-7 id="' . get_theme_mod ( 'signup' ) . '" title="Записаться"]' ); ?>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="modal custom fade" id="order_call" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <?php echo do_shortcode( '[contact-form-7 id="' . get_theme_mod ( 'order_call' ) . '" title="Записаться"]' ); ?>
+    <div class="modal custom fade" id="order_call" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <?php echo do_shortcode( '[contact-form-7 id="' . get_theme_mod ( 'order_call' ) . '" title="Записаться"]' ); ?>
+        </div>
       </div>
     </div>
   </div>
